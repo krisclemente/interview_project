7e5aa1759d1a4a3bb3edd40450b17863920700d5
Kris <kris.clemente@lootcrate.com>
2015-08-10 10:18:35 -0700

    Adding an array test and deleting the other two.

---
+++ array2.rb
@@ -0,0 +1,20 @@
+# Given what you now know about arrays.  Take the given array of integers here:
+
+integers = [1, 4, 6, 16, 30]
+integers2 = [15, 2, 30, 17, 8, 20, 44]
+
+# Complete the function below so that it returns the average of the numbers above
+
+def average( arr )
+  #CODE HERE
+end
+
+puts "Average of integers:"
+puts average(integers)
+puts "Average of integers2:"
+puts average(integers2)
+
+#HINTS: 
+# You'll need to iterage over each integer in the array
+# You'll need to convert the sum into a float (decimal) 
+# in order to get an an average that is also in float.
