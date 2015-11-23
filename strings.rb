f68720adff1d6fefe4c05429613776b52fdcc57e
Kris <kris.clemente@lootcrate.com>
2015-11-23 10:02:57 -0800

    Adding an anagram problem.

--- strings.rb
+++ strings.rb
@@ -1,4 +1,6 @@
 # STRING MANIPULATION
+
+# ** PALINDROME **
 # A "palindrome" is a string that reads the same forwards and backwards.
 # An example of a palindrome is the string "ABBA" - If you reverse the string it is still "ABBA"
 
@@ -8,14 +10,29 @@
 #Complete this function so that it will corretly return true or false for the strings below.
 
 def palindrome(string)
+  puts "is #{string} a palindrome?"
   #CODE GOES HERE
 end
 
-puts "is ABBA a palindrome?"
 puts palindrome('ABBA')
-puts "is FISH a palindrome?"
 puts palindrome('FISH')
-puts "is ABBCBBA a palindrome?"
 puts palindrome('ABBCBBA')
-puts "is ABBBCDBBBA a palindrome?"
 puts palindrome('ABBBCDBBBA')
+
+# ** ANAGRAM **
+# Two strings are considered anagrams if they contain the exact same characters in a different order.
+# An example of two strings that are anagrams are "army" and "mary"
+
+#Below is a ruby function that takes in two strings and returns true if they are anagrams and false otherwise
+
+#Complete this function so that is will correctly return true of false for the strings below
+
+def anagram(string1, string2)
+  puts "Are #{string1} and #{string2} anagrams?"
+  #CODE GOES HERE
+end
+
+puts anagram('mary', 'army')
+puts anagram('dot', 'spot')
+puts anagram('sunny', 'runny')
+puts anagram('wash', 'shaw')
